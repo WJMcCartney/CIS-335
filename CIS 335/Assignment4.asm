6c9da47e34e33cd5db2969c5babc7fa8a634bd4f
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-11-25 17:06:35 -0500

    working program with debugging lines

--- CIS 335/Assignment4.asm
+++ CIS 335/Assignment4.asm
@@ -2,19 +2,16 @@
 ; Program Name: CIS 335 Assignement 5
 ; Program Description: randomly generate a color by set values
 ; Date: 30 November 2022 
-
 INCLUDE Irvine32.inc
-
 .data
 intArr SDWORD 20 DUP(?); creates an uninitialized array with type SDWORD (4 bytes)
 msg byte "this is a string made up of random colors"
-
+testingVar SDWORD ?
 .code
 main PROC
 call Randomize; gets a random seed
 mov esi, OFFSET intArr; finds the location of first element of intArr
 mov ecx, LENGTHOF intArr; how many times will it loop
-
 Loop1:
 mov eax, 10;
 Call RandomRange; gets a random value from the seed
@@ -26,7 +23,14 @@ add esi, 4
 loop Loop1
 mov esi, OFFSET intArr
 mov ecx, LENGTHOF intArr
+inc ecx; starting the counter at +1 so that we can decrement immediately because the loops did not work
 Loop2:
+dec ecx
+mov testingVar, ecx
+mov eax, testingVar
+call WriteDec
+cmp ecx, 0
+je EndProgramHere; end the loop if it reaches here
 cmp SDWORD ptr[esi], 10; this compares whether the contents of esi is less than, equal to, or greater than 10 and is blue
 jge Loop3; if it is greater than or equal to 0 then it will not complete the increment count
 cmp SDWORD ptr[esi], 7; this compares whether the contents of esi is less than, equal to, or greater than 7 and is white
@@ -34,33 +38,31 @@ jge Loop4; if it is not white or blue then it is green
 cmp SDWORD ptr[esi], 1
 je Loop5; we may be able to run all these different jumps and loops as different methods to call (Call GreenCountIncrease or some shit like that)
 Loop5:
-mov eax, green
-Call SetTextColor
-mov edx, OFFSET msg
-Call WriteString
-CALL CRLF
+mov eax, green; last jump goes here and sets eax to irvine32 green
+Call SetTextColor; uses irvine32 to set the color to green
+mov edx, OFFSET msg; sets edx to our message
+Call WriteString; writes the message
+CALL CRLF; sets it to a new line
 add esi, 4
-jmp Loop2
+jmp Loop2; go back to beginning of loop
 Loop4:
-mov eax, white
-Call SetTextColor
-mov edx, OFFSET msg
-Call WriteString
-CALL CRLF
+mov eax, white; second jump goes here and sets eax to irvine32 white
+Call SetTextColor; uses irvine32 to set the color to green
+mov edx, OFFSET msg; sets edx to our message
+Call WriteString; writes the message
+CALL CRLF; sets it to a new line
 add esi, 4
 jmp Loop2
 Loop3:
-mov eax, blue
-Call SetTextColor
-mov edx, OFFSET msg
-Call WriteString
-CALL CRLF
+mov eax, blue; first jump goes here and sets eax to irvine32 blue
+Call SetTextColor; uses irvine32 to set the color to green
+mov edx, OFFSET msg; sets edx to our message
+Call WriteString; writes the message
+CALL CRLF; sets it to a new line
 add esi, 4
 jmp Loop2
-;add a way for the numbers to correspond to the string i think
-
+EndProgramHere:
 ;end the program
-
  exit
 main ENDP
 END main
