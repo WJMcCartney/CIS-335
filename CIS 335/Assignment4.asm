a79d9250b5064236940fb1cd17976c403aa4d4a2
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-11-28 12:09:30 -0500

    added comments to some more lines

--- CIS 335/Assignment4.asm
+++ CIS 335/Assignment4.asm
@@ -1,6 +1,6 @@
 ; Author: William McCartney
 ; Program Name: CIS 335 Assignement 5
-; Program Description: randomly generate a color by set values
+; Program Description: randomly generates color values and prints a string with that color.
 ; Date: 30 November 2022 
 INCLUDE Irvine32.inc
 .data
@@ -23,15 +23,15 @@ mov esi, OFFSET intArr
 mov ecx, LENGTHOF intArr
 inc ecx; starting the counter at +1 so that we can decrement immediately because the loops did not work
 Loop2:
-dec ecx
-cmp ecx, 0
+dec ecx; this is not the proper way to use loops but the loop was not working so i manually dec ecx and then just jump to Loop2
+cmp ecx, 0; this checks to end the loop
 je EndProgramHere; end the loop if it reaches here
 cmp SDWORD ptr[esi], 10; this compares whether the contents of esi is less than, equal to, or greater than 10 and is blue
 jge Loop3; if it is greater than or equal to 0 then it will not complete the increment count
 cmp SDWORD ptr[esi], 7; this compares whether the contents of esi is less than, equal to, or greater than 7 and is white
 jge Loop4; if it is not white or blue then it is green
-cmp SDWORD ptr[esi], 1
-je Loop5; we may be able to run all these different jumps and loops as different methods to call (Call GreenCountIncrease or some shit like that)
+cmp SDWORD ptr[esi], 1; can remove this statement and just have it jump to loop 5 if neither option happens but it looks nicer with it all being the same
+je Loop5; we may be able to run all these different jumps and loops as different procedures to call (Call GreenCountIncrease or some shit like that)
 Loop5:
 mov eax, green; last jump goes here and sets eax to irvine32 green
 Call SetTextColor; uses irvine32 to set the color to green
@@ -57,7 +57,6 @@ CALL CRLF; sets it to a new line
 add esi, 4
 jmp Loop2
 EndProgramHere:
-;end the program
  exit
-main ENDP
+main ENDP; end the program
 END main
